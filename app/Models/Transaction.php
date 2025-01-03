f2f89343388e7af56679e7569da4f2c022877ccb
aidilrx04 <mrcrayv14@gmail.com>
2025-01-03 15:26:39 +0800

    make transaction sort by latest by default

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\SortByLatest;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+#[ScopedBy(SortByLatest::class)]
 class Transaction extends Model
 {
     /** @use HasFactory<\Database\Factories\TransactionFactory> */
