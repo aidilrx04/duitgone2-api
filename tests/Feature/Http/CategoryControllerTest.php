28831b81f040722315e89f2a9681948d019bf6e4
aidilrx04 <mrcrayv14@gmail.com>
2025-01-03 11:27:58 +0800

    add category controller test

---
+++ tests/Feature/Http/CategoryControllerTest.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Http;
+
+use App\Models\Category;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CategoryControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function it_can_list_all_categories()
+    {
+        Category::factory()->count(3)->create();
+
+        $response = $this->getJson(route('api.categories.index'));
+
+        $response
+            ->assertOk()
+            ->assertJsonCount(3);
+    }
+
+    public function it_can_store_a_category()
+    {
+        $data = [
+            'label' => 'Test Category',
+        ];
+
+        $response = $this->postJson(route('api.categories.store'), $data);
+
+        $response
+            ->assertCreated()
+            ->assertJsonFragment($data);
+
+        $this->assertDatabaseHas('categories', $data);
+    }
+
+    public function it_can_show_a_category()
+    {
+        $category = Category::factory()->create();
+
+        $response = $this->getJson(route('api.categories.show', $category));
+
+        $response
+            ->assertOk()
+            ->assertJsonFragment([
+                'id' => $category->id,
+                'label' => $category->label,
+            ]);
+    }
+
+    public function it_can_update_a_category()
+    {
+        $category = Category::factory()->create();
+
+        $data = [
+            'label' => 'Updated Category',
+        ];
+
+        $response = $this->putJson(route('api.categories.update', $category), $data);
+
+        $response
+            ->assertOk()
+            ->assertJsonFragment($data);
+
+        $this->assertDatabaseHas('categories', $data);
+    }
+
+    public function it_can_delete_a_category()
+    {
+        $category = Category::factory()->create();
+
+        $response = $this->deleteJson(route('api.categories.destroy', $category));
+
+        $response->assertOk();
+
+        $this->assertDatabaseMissing('categories', [
+            'id' => $category->id,
+        ]);
+    }
+}
