e3b716e6635d3ea633aa9bf8931cf3fd7826c993
aidilrx04 <mrcrayv14@gmail.com>
2025-01-03 15:28:05 +0800

    scope category sort by latest

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\SortByLatest;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+#[ScopedBy(SortByLatest::class)]
 class Category extends Model
 {
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
