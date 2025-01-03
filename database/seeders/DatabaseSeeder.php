71fd886eb2103df4253222fa2b173cb838a2a49a
aidilrx04 <mrcrayv14@gmail.com>
2025-01-03 15:02:53 +0800

    seed with category for transaction models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,8 +21,8 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
+            CategorySeeder::class,
             TransactionSeeder::class,
-            CategorySeeder::class
         ]);
     }
 }
--- database/seeders/TransactionSeeder.php
+++ database/seeders/TransactionSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Transaction;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,6 +16,6 @@ class TransactionSeeder extends Seeder
     {
         $total = 20;
 
-        Transaction::factory()->count($total)->create();
+        Transaction::factory()->for(Category::inRandomOrder()->first())->count($total)->create();
     }
 }
