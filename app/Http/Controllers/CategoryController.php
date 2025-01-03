5370a20429a0630f89b5670d8578dd6ea4b0c985
aidilrx04 <mrcrayv14@gmail.com>
2025-01-03 16:24:11 +0800

    add sort and include transactions to category query builder

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,7 +7,9 @@ use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Requests\UpdateCategoryRequest;
 use App\Support\InvalidRequestResponse;
 use Exception;
+use Illuminate\Support\Facades\Log;
 use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\AllowedInclude;
 use Spatie\QueryBuilder\QueryBuilder;
 
 class CategoryController extends Controller
@@ -20,10 +22,12 @@ class CategoryController extends Controller
         try {
             $categories = QueryBuilder::for(Category::class)
                 ->allowedFilters(['label', AllowedFilter::exact('id')])
+                ->allowedSorts(['id', 'label', 'created_at', 'updated_at'])
+                ->allowedIncludes(['transactions', AllowedInclude::count('transactionsCount')])
                 ->get();
             return $categories;
         } catch (Exception $e) {
-            report($e);
+            Log::error($e);
             return InvalidRequestResponse::notAllowed();
         }
     }
